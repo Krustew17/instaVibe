af7774c3fda65f6e72063cee802c829e7e26235a
krustew17 <krasimircho555@gmail.com>
2024-07-27 00:43:36 +0300

    added post details and post delete (image deleted from cloudinary aswell)

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,4 +1,9 @@
-import { createPost } from "../controllers/posts.controller.js";
+import {
+    createPost,
+    getAllPosts,
+    updatePost,
+    deletePost,
+} from "../controllers/posts.controller.js";
 import { upload } from "../configs/multer.js";
 import express from "express";
 import { authMiddleware } from "../middlewares/auth.middleware.js";
@@ -6,5 +11,7 @@ import { authMiddleware } from "../middlewares/auth.middleware.js";
 const router = express.Router();
 
 router.post("/create", authMiddleware, upload.single("image"), createPost);
-
+router.get("/all", getAllPosts);
+router.put("/:id/update", authMiddleware, updatePost);
+router.delete("/:id/delete", authMiddleware, deletePost);
 export default router;
