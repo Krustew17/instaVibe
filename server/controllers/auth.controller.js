fd3a11edf311e5ba754b255b5b6dee8f062e9683
krustew17 <krasimircho555@gmail.com>
2024-08-08 16:48:08 +0300

    added forgotten import

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/User.js";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
+import validateUsername from "../utils/validateUsername.js";
 
 // REGISTER USER
 export const register = async (req, res) => {
